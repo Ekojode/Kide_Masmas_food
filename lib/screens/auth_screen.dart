63c213495e012a7779f139d14a8fcce98511acf1
Ekojode <58553973+Ekojode@users.noreply.github.com>
2022-07-16 14:17:07 +0100

    Finished Auth Screen UI

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:masmas_food/screens/on_boarding_2.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+
+import '../global/app_text_style.dart';
+import '../global/app_color.dart';
+
+import '../widgets/button.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
@@ -11,38 +17,276 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _formKey = GlobalKey<FormState>();
+  AuthMode authMode = AuthMode.signIn;
+  bool _passwordObscure = true;
+  bool _passwordConfirm = true;
+  bool _keepSignedIn = false;
+  bool _addToEmailList = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          //   SizedBox(height: 5.h),
-          SizedBox(
-            height: 45.h,
-            child: Stack(
-              children: <Widget>[
-                Image.asset(
-                  "assets/images/Pattern.png",
-                  fit: BoxFit.cover,
-                ),
-                Container(
-                  margin: EdgeInsets.all(10.h),
-                  child: Image.asset(
-                    "assets/images/Logo.png",
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            //   SizedBox(height: 5.h),
+            SizedBox(
+              height: 42.h,
+              child: Stack(
+                children: <Widget>[
+                  Image.asset(
+                    "assets/images/Pattern.png",
                     fit: BoxFit.cover,
                   ),
-                ),
-              ],
+                  Container(
+                    margin: EdgeInsets.all(10.h),
+                    child: Image.asset(
+                      "assets/images/Logo.png",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 35.h,
-          ),
-          SizedBox(
-            height: 20.h,
-          )
-        ],
+            SizedBox(
+              height: 45.h,
+              child: Form(
+                  key: _formKey,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 7.5.w),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      //    crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          authMode == AuthMode.signIn
+                              ? "Login To Your Account"
+                              : "Sign Up For Free",
+                          style: textStyle1,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              border: Border.all(color: Colors.grey)),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                                hintText: "Email",
+                                border: InputBorder.none,
+                                icon: Icon(Icons.mail)),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              border: Border.all(color: Colors.grey)),
+                          child: TextFormField(
+                            obscureText: _passwordObscure,
+                            decoration: InputDecoration(
+                              hintText: "Password",
+                              border: InputBorder.none,
+                              icon: const Icon(Icons.lock),
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  if (_passwordObscure) {
+                                    setState(() {
+                                      _passwordObscure = false;
+                                    });
+                                  } else {
+                                    setState(() {
+                                      _passwordObscure = true;
+                                    });
+                                  }
+                                },
+                                icon: Icon(_passwordObscure
+                                    ? Icons.remove_red_eye
+                                    : Icons.visibility_off),
+                              ),
+                            ),
+                          ),
+                        ),
+                        authMode == AuthMode.signIn
+                            ? Text(
+                                "Or Continue with",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16.sp),
+                              )
+                            : Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                    border: Border.all(color: Colors.grey)),
+                                child: TextFormField(
+                                  obscureText: _passwordConfirm,
+                                  decoration: InputDecoration(
+                                    hintText: "Confirm Password",
+                                    border: InputBorder.none,
+                                    icon: const Icon(Icons.lock),
+                                    suffixIcon: IconButton(
+                                      onPressed: () {
+                                        if (_passwordConfirm) {
+                                          setState(() {
+                                            _passwordConfirm = false;
+                                          });
+                                        } else {
+                                          setState(() {
+                                            _passwordConfirm = true;
+                                          });
+                                        }
+                                      },
+                                      icon: Icon(_passwordConfirm
+                                          ? Icons.remove_red_eye
+                                          : Icons.visibility_off),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                        authMode == AuthMode.signIn
+                            ? Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Container(
+                                        height: 6.h,
+                                        width: 40.w,
+                                        decoration: BoxDecoration(
+                                            color: const Color(0xffF4F4F4),
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Icon(
+                                              Icons.facebook,
+                                              color: Colors.blue[700],
+                                            ),
+                                            const Text(" Facebook")
+                                          ],
+                                        ),
+                                      ),
+                                      const Spacer(),
+                                      Container(
+                                        height: 5.h,
+                                        width: 40.w,
+                                        decoration: BoxDecoration(
+                                            color: const Color(0xffF4F4F4),
+                                            borderRadius:
+                                                BorderRadius.circular(10)),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Image.asset(
+                                                "assets/images/google-icon 1.png"),
+                                            const Text(" Google")
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context)
+                                          .pushReplacementNamed(
+                                              OnBoarding2.routeName);
+                                    },
+                                    child: Text(
+                                      "Forgot your Password?",
+                                      style: TextStyle(
+                                        fontSize: 17.sp,
+                                        fontWeight: FontWeight.bold,
+                                        color: greenColor1,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              )
+                            : Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Transform.scale(
+                                        scale: 1.3,
+                                        child: Checkbox(
+                                            shape: const CircleBorder(),
+                                            value: _keepSignedIn,
+                                            onChanged: (value) {
+                                              setState(() {
+                                                _keepSignedIn = value!;
+                                              });
+                                            }),
+                                      ),
+                                      const Text("Keep me Signed In")
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Transform.scale(
+                                        scale: 1.3,
+                                        child: Checkbox(
+                                            shape: const CircleBorder(),
+                                            value: _addToEmailList,
+                                            onChanged: (value) {
+                                              setState(() {
+                                                _addToEmailList = value!;
+                                              });
+                                            }),
+                                      ),
+                                      const Text(
+                                          "Email me about special updates")
+                                    ],
+                                  )
+                                ],
+                              ),
+                      ],
+                    ),
+                  )),
+            ),
+            SizedBox(
+              height: 13.h,
+              child: Column(
+                children: [
+                  Button(
+                      text: authMode == AuthMode.signIn
+                          ? "Login"
+                          : "Create Account",
+                      onTap: () {},
+                      height: 6.h,
+                      width: authMode == AuthMode.signIn ? 30.w : 40.w),
+                  TextButton(
+                    onPressed: () {
+                      if (authMode == AuthMode.signIn) {
+                        setState(() {
+                          authMode = AuthMode.signUp;
+                        });
+                      } else {
+                        setState(() {
+                          authMode = AuthMode.signIn;
+                        });
+                      }
+                    },
+                    child: Text(
+                      authMode == AuthMode.signIn
+                          ? "Don't have an account yet?, Sign Up"
+                          : "Already have an account?, Sign In",
+                      style: TextStyle(
+                          fontSize: 17.sp,
+                          fontWeight: FontWeight.bold,
+                          color: greenColor1),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
+}
+
+enum AuthMode {
+  signUp,
+  signIn,
 }
